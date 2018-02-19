003a342b9d7777b1396a2ca77ceef6fca5389397
bestrocker221 <albertoscola221@gmail.com>
2018-02-19 13:28:23 +0100

    added update-host file

---
+++ update-hosts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Check if the hosts file contains the actual current IP/domain of the needed host
+#
+# Edit DOMAIN field.
+DOMAIN=
+IP=`cat /etc/hosts | grep bestrockers | awk '{print $1}'`
+NEW_IP=`nslookup $DOMAIN | grep Address | tail -n 1 | awk '{print $2}'`
+
+
+if [ $IP == $NEW_IP ]; then
+	echo `date` "NOCHANGE hosts="$DOMAIN "ip="$IP >> /var/log/checkrules.log
+else
+	sed -i -e "s/${IP}/${NEW_IP}/g" /etc/hosts
+	echo `date` "CHANGED hosts="$DOMAIN "ip="$IP "newIP="$NEW_IP >> /var/log/checkrules.log
+fi
