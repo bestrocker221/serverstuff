4fad0c3d267409863fefc459c5fd809d57d2848b
bestrocker221 <albertoscola221@gmail.com>
2018-05-05 21:58:52 +0200

    added sqldump commands

--- full_mysql_backup.sh
+++ full_mysql_backup.sh
@@ -8,3 +8,25 @@ USER=
 PASSWORD=
 
 mysqldump -u ${USER} --password=${PASSWORD} -A -R -E --triggers --single-transaction > full_backup_`date | sed -e 's/ //g'`.mysql
+
+
+# Copy one/more database
+# mysqldump -u user -p db > backup.sql
+# mysqldump -u user -p --databases db1 db2 db3> backup.sql
+# mysqldump -u user -p --all-databases > backup.sql
+
+#Only one table
+#mysqldump -u user -p db table > backup.sql
+#mysqldump -u user -p --where="id > 10" db table > backup.sql
+
+# Compress db
+#mysqldump -u user -p db_da_copiare | gzip -9 > backup.sql.gz
+
+
+## How to Recover backup
+
+#mysql -u user -p < backup.sql
+#mysql -u user -p --one-database db < file_with_all_db.sql
+
+#From gz archive
+#gunzip < backup.sql.gz | mysql -u user -p
